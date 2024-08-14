ad224abae92e50005c621395e489a6b40dbf2f5d
Bruno Duarte Pereira Santos <duartebrunops@gmail.com>
2024-08-14 09:30:27 -0300

    Project created.

---
+++ MyTripChum/MyTripChumApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTripChumApp.swift
+//  MyTripChum
+//
+//  Created by Bruno Duarte on 14/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MyTripChumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
